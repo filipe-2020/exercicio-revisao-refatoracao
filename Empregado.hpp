26cd98e19c29ec44090006a10b66ec32c7e1c877
filipe-2020 <73084478+filipe-2020@users.noreply.github.com>
2020-10-20 23:59:18 -0300

    Update Empregado.hpp

--- Empregado.hpp
+++ Empregado.hpp
@@ -1,28 +1,34 @@
 #ifndef EMPREGADO_H
 #define EMPREGADO_H
-
 #include <iostream>
 #include <string>
 
-class Empregado {
-	
-  public:
-    double salarioHora;  
-    double quotaMensalVendas;  
+class Empregado
+{
+
+protected:
+
+  std::string nome;
+  double salarioHora;
+  double horasDeTrabalho;
 
 
-    double pagamentoMes(double horasTrabalhadas) {
- 
-      double t = horasTrabalhadas;
-	  
-	  //Cálculo de hora extra (+50% se horasTrabalhadas > 8)
-      if (horasTrabalhadas > 8) {
-        double x = horasTrabalhadas - 8;
-        t += x / 2;
-      }
-	  return t * salarioHora;
+  double pagamentoMes (double horasTrabalhadas)
+  {
+
+    double horasDeTrabalhoCorrigido = horasTrabalhadas;
+
+    //calculo de hora extra (+50% se horasTrabalhadas > 8)
+    if (horasTrabalhadas > 8)
+    {
+	  double horasExtras = horasTrabalhadas - 8;
+	  horasDeTrabalhoCorrigido += (horasExtras / 2);
     }
-	
+    //Retorna o salario corrigido
+    return (horasDeTrabalhoCorrigido * salarioHora);
+  }
+
+
 };
 
 #endif
